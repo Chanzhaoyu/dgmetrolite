c64eca2a77eb3cdb23f8618a44b3e155c5839947
Chanzhaoyu <790348264@qq.com>
2018-07-26 20:34:32 +0800

    07262033

--- app/pages/metro/index.js
+++ app/pages/metro/index.js
@@ -1,25 +1,21 @@
 let stationData = require('../../utils/station_data.js');
 
 Page({
-
   data: {
     bannerUrls: [
-      '/static/images/banner_01.jpg'
+      '/static/images/logo_01.png'
     ],
-    line:['2号线'],
-    key: 0,
-    tabIndex: 0
+    tabIndex: 0,
+    /*站点遮罩数据*/
+    stationName: '东莞火车站',
+    stationKey: 0,
+    modalFlag: true,
+    lineIndex: 0
   },
   onLoad(options) {
     let localData = stationData.station;
-    let filterStation = [];
-    // 站点数组
-    for (let i = 0; i < localData.length; i++) {
-      filterStation.push(localData[i].name);
-    }
     this.setData({
-      station: localData,
-      filterStation: filterStation
+      station: localData
     })
   },
   bindPickerChange(e) {
@@ -36,23 +32,45 @@ Page({
     let lat = parseFloat(e.currentTarget.dataset.lat);
     let lon = parseFloat(e.currentTarget.dataset.lon);
     let name = e.currentTarget.dataset.name;
+    let en_name = e.currentTarget.dataset.en;
     wx.openLocation({
       name: name,
+      address: en_name,
       latitude: lat,
       longitude: lon,
       scale: 24
     })
   },
-  viewLine(e){
+  viewLine(e) {
     let imgUrl = e.currentTarget.dataset.url;
     wx.previewImage({
       urls: [imgUrl]
     })
   },
+  // 线路遮罩
+  modalShow() {
+    this.setData({
+      modalFlag: false
+    })
+  },
+  // 线路切换
+  lineBind(e) {
+    this.setData({
+      lineIndex: e.currentTarget.dataset.index
+    })
+  },
+  // 站点选中
+  stationBind(e) {
+    this.setData({
+      stationName: e.currentTarget.dataset.name,
+      stationKey: e.currentTarget.dataset.key,
+      modalFlag: true
+    })
+  },
   onShareAppMessage(e) {
     return {
       title: '东莞地铁信息',
-      path: '/pages/index/index'
+      path: '/pages/metro/index'
     }
   }
 })
---
+++ app/pages/station_view/index.js
@@ -0,0 +1,77 @@
+let stationData = require('../../utils/station_data.js');
+
+Page({
+  data: {
+    bannerUrls: [
+      '/static/images/logo_01.png'
+    ],
+    tabIndex: 0,
+    /*站点遮罩数据*/
+    stationName: '东莞火车站',
+    stationKey: 0,
+    modalFlag: true,
+    lineIndex: 0
+  },
+  onLoad(options) {
+    let localData = stationData.station;
+    this.setData({
+      station: localData
+    })
+  },
+  bindPickerChange(e) {
+    this.setData({
+      key: e.detail.value
+    })
+  },
+  tabChange(e) {
+    this.setData({
+      tabIndex: e.currentTarget.dataset.index
+    })
+  },
+  toStation(e) {
+    let lat = parseFloat(e.currentTarget.dataset.lat);
+    let lon = parseFloat(e.currentTarget.dataset.lon);
+    let name = e.currentTarget.dataset.name;
+    let en_name = e.currentTarget.dataset.en;
+    wx.openLocation({
+      name: name,
+      address: "点击右侧按钮开启地图导航",
+      latitude: lat,
+      longitude: lon,
+      scale: 24
+    })
+  },
+  // 线路预览
+  viewLine(e) {
+    let imgUrl = e.currentTarget.dataset.url;
+    wx.previewImage({
+      urls: [imgUrl]
+    })
+  },
+  // 线路遮罩
+  modalShow() {
+    this.setData({
+      modalFlag: false
+    })
+  },
+  // 线路切换
+  lineBind(e) {
+    this.setData({
+      lineIndex: e.currentTarget.dataset.index
+    })
+  },
+  // 站点选中
+  stationBind(e) {
+    this.setData({
+      stationName: e.currentTarget.dataset.name,
+      stationKey: e.currentTarget.dataset.key,
+      modalFlag: true
+    })
+  },
+  onShareAppMessage(e) {
+    return {
+      title: '东莞地铁信息',
+      path: '/pages/metro/index'
+    }
+  }
+})
